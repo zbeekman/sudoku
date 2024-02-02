263e3bc1a1dffd7c6c6c78b5e35c369f1c3ac3c9
Izaak Beekman <zbeekman@gmail.com>
2024-02-02 13:37:04 -0500

    Improve formatting of __main__ and test_sudoku.py w/ black

--- src/sudokutoolbox/__main__.py
+++ src/sudokutoolbox/__main__.py
@@ -1,3 +1,4 @@
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sudokutoolbox.sudoku import main
+
     main()
--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -4,7 +4,6 @@ from sudokutoolbox import Sudoku
 
 
 class TestSudoku:
-
     def test_puzzle_default(self):
         puz = Sudoku()
         assert puz.puzzle.shape == (9, 9), "Default puzzle shape is not (9, 9)"
@@ -18,4 +17,6 @@ class TestSudoku:
         nyt_hard = "..2........59.3.7......69......8....19....83..4.6....3...2.7.1.57..4....8..3....."
         puz = Sudoku(nyt_hard)
         assert puz.puzzle.shape == (9, 9), "Puzzle shape is not (9, 9)"
-        assert all([int(cell) in set(range(puz._N+1)) for cell in puz.puzzle.flatten()]), "Puzzle contains invalid cell values"
+        assert all(
+            [int(cell) in set(range(puz._N + 1)) for cell in puz.puzzle.flatten()]
+        ), "Puzzle contains invalid cell values"
