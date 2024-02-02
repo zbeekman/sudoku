b69356dd5452e2dd2c78591ac366586307bcdd71
Izaak Beekman <zbeekman@gmail.com>
2024-02-02 11:43:04 -0500

    Clean-ish numpy & bitmask implementation
     - Numpy arrays for working constraints, puzzle and solution
     - Uses bit masking rather than python sets
     - Does not do any optimization of solve order or skipping filled in
       cells
    
     Timing output
     -------------
    
     ```
     real    0m2.326s
     user    0m2.878s
     sys     0m0.362s
     ```

---
+++ src/sudoku-utils/sudoku.py
@@ -0,0 +1,157 @@
+from typing import Union, List, Tuple
+import numpy as np
+import numpy.typing as npt
+import math
+from attrs import define, field, Factory, cmp_using, setters, validators
+
+_1 = np.int_(1)
+def is_square(N: int) -> bool:
+    return math.sqrt(N).is_integer()
+
+def valid_square(instance, attribute, value):
+    if not is_square(value):
+        raise ValueError(f"{attribute.name} of {instance.name} must have an integer square root.")
+
+@define(kw_only=True)
+class Constraint:
+    N: int = field(default=9, on_setattr=setters.frozen, validator=[validators.instance_of(int), validators.gt(0), valid_square]) # type: ignore
+    _RN: int = field(init=False, default=Factory(lambda self: int(math.sqrt(self.N)), takes_self=True))
+    r: tuple[int, ...] = field(
+        init=False,
+        repr=False,
+        default=Factory(lambda self: tuple(i//self.N for i in range(self.N**2)), takes_self=True))
+    c: tuple[int, ...] = field(
+        init=False,
+        repr=False,
+        default=Factory(lambda self: tuple(i%self.N for i in range(self.N**2)), takes_self=True))
+    b: tuple[tuple[int, int], ...] = field(
+        init=False,
+        repr=False,
+        default=Factory(lambda self: tuple((self.r[i]//self._RN,self.c[i]//self._RN) for i in range(self.N**2)), takes_self=True))
+    row: npt.NDArray[np.int_] = field(
+        converter=np.array,
+        eq=cmp_using(np.array_equal),
+        default=Factory(lambda self: np.zeros(self.N, dtype=np.int_), takes_self=True))
+    col: npt.NDArray[np.int_] = field(
+        converter=np.array,
+        eq=cmp_using(np.array_equal),
+        default=Factory(lambda self: np.zeros(self.N, dtype=np.int_), takes_self=True))
+    box: npt.NDArray[np.int_] = field(
+        converter=np.array,
+        eq=cmp_using(np.array_equal),
+        default=Factory(lambda self: np.zeros(tuple(self._RN for _ in range(2)), dtype=np.int_), takes_self=True))
+
+    @classmethod
+    def from_puzzle(cls, puzzle: npt.NDArray[np.int_]) -> 'Constraint':
+        if not is_square(puzzle.size):
+            raise ValueError("Puzzle must be a perfect square.")
+        root_n = int(math.sqrt(puzzle.size))
+        root_rn = int(math.sqrt(root_n))
+        _row=np.zeros((root_n), dtype=np.int_)
+        _col=np.zeros((root_n), dtype=np.int_)
+        _box=np.zeros((root_rn, root_rn), dtype=np.int_)
+        for i, j in np.ndindex(puzzle.shape):
+            if puzzle[i, j] != 0:
+                _row[i] |= _1 << puzzle[i, j]
+                _col[j] |= _1 << puzzle[i, j]
+                _box[i//root_rn, j//root_rn] |= _1 << puzzle[i, j]
+        return cls(N=root_n, row=_row, col=_col, box=_box)
+
+    def is_safe(self, i: int, v: np.int_) -> bool:
+        """If the constraint isn't already present, update the constraint and return True."""
+        shifted: np.int_ = _1 << v
+        if ((self.row[self.r[i]] & shifted) or
+            (self.col[self.c[i]] & shifted) or
+            (self.box[self.b[i]] & shifted)):
+            return False
+        return True
+
+    def update(self, i: int, v: np.int_) -> None:
+        shifted: np.int_ = _1 << v
+        self.row[self.r[i]] |= shifted
+        self.col[self.c[i]] |= shifted
+        self.box[self.b[i]] |= shifted
+
+    def remove(self, i: int, v: np.int_) -> None:
+        """Remove the constraint."""
+        shifted: np.int_ = _1 << v
+        self.row[self.r[i]] &= ~shifted
+        self.col[self.c[i]] &= ~shifted
+        self.box[self.b[i]] &= ~shifted
+
+
+def _convert(input: Union[str, npt.NDArray[np.int_], Tuple[int, ...], Tuple[Tuple[int, ...]], List[int], List[List[int]]]) -> npt.NDArray[np.int_]:
+    """Converts input to a 2D numpy array of integers.
+    "." and "0" are treated as empty cells.
+    """
+    _n: int = 0
+    if isinstance(input, str):
+        input = input.replace(",", "")
+        input = "".join(input.split())
+        input = input.replace(".", "0")
+        input = np.array([int(c,base=36) for c in input]) # Allow for 36x36 puzzles
+    elif isinstance(input, (tuple, list)):
+        input = np.array(input)
+    if input.ndim == 1:
+        if not is_square(input.size):
+            raise ValueError(f"Input string length ({len(input)}) is not a perfect square.")
+        _n = int(math.sqrt(input.size))
+        input = input.reshape((_n, _n))
+    return input
+
+@define
+class Sudoku:
+    _N: int = field(default=9,
+                    on_setattr=setters.frozen,
+                    validator=[validators.instance_of(int), validators.gt(0), valid_square], # type: ignore
+                    alias="_N",
+                    kw_only=True)
+    puzzle: npt.NDArray[np.int_] = field(eq=cmp_using(eq=np.array_equal),
+                                         converter=_convert,
+                                         on_setattr=setters.frozen,
+                                         default=Factory(lambda self: np.zeros((self._N, self._N), dtype=np.int_), takes_self=True))
+    solution: npt.NDArray[np.int_] = field(eq=cmp_using(eq=np.array_equal),
+                                           init=False,
+                                           default=Factory(lambda self: self.puzzle.flatten(), takes_self=True))
+    constraint: Constraint = field(init=False,
+                                   default=Factory(lambda self: Constraint.from_puzzle(self.puzzle), takes_self=True),
+                                   repr=False)
+    candidates: list[list[np.int_]] = field(init=False, on_setattr=setters.frozen)
+    @candidates.default # type: ignore
+    def _set_candidates(self) -> list[list[int]]:
+        frequency = [0 for _ in range(self._N)]
+        c = [list() for _ in self.puzzle.flatten()]
+        for i, j in np.ndindex(self.puzzle.shape):
+            if self.puzzle[i, j] != 0:
+                continue
+            for k in range(self._N):
+                if self.constraint.is_safe(i*self._N + j, k+_1):
+                    c[i*self._N + j].append(np.int_(k+1))
+                    frequency[k-1] += 1
+        # for possible in c:
+        #     possible.sort(key=lambda x: frequency[x-1],reverse=True)
+        return c
+
+    # install line_profiler with pip install line_profiler
+    # kernprof -l -v sudoku.py to get a line-by-line profile
+    # @profile
+    def solve(self, i) -> bool:
+        if i >= self._N**2:
+            return True
+        if self.solution[i] != 0:
+            return self.solve(i+1)
+        for v in self.candidates[i]:
+            if self.constraint.is_safe(i, v):
+                self.solution[i] = v
+                self.constraint.update(i, v)
+                if self.solve(i+1):
+                    return True
+                self.solution[i] = 0
+                self.constraint.remove(i, v)
+        return False
+
+if __name__ == '__main__':
+    nyt_hard = "...2........59.3.7......69......8....19....83..4.6....3...2.7.1.57..4....8..3...."
+    puz4 = Sudoku(nyt_hard)
+    puz4.solve(0)
+    print(puz4)
---
+++ tests/test_constraints.py
@@ -0,0 +1,145 @@
+import pytest
+import numpy as np
+import copy
+from sudoku import Sudoku, Constraint
+
+_1 = np.int_(1)
+
+_nyt_hard = np.array([
+            [0,0,0, 2,0,0, 0,0,0],
+            [0,0,0, 5,9,0, 3,0,7],
+            [0,0,0, 0,0,0, 6,9,0],
+
+            [0,0,0, 0,0,8, 0,0,0],
+            [0,1,9, 0,0,0, 0,8,3],
+            [0,0,4, 0,6,0, 0,0,0],
+
+            [3,0,0, 0,2,0, 7,0,1],
+            [0,5,7, 0,0,4, 0,0,0],
+            [0,8,0, 0,3,0, 0,0,0],
+    ], dtype=np.int_)
+
+_nyth_sol = np.array([
+        [9,6,5, 2,7,3, 8,1,4],
+        [1,4,8, 5,9,6, 3,2,7],
+        [7,2,3, 4,8,1, 6,9,5],
+
+        [5,7,2, 3,4,8, 1,6,9],
+        [6,1,9, 7,5,2, 4,8,3],
+        [8,3,4, 1,6,9, 5,7,2],
+
+        [3,9,6, 8,2,5, 7,4,1],
+        [2,5,7, 6,1,4, 9,3,8],
+        [4,8,1, 9,3,7, 2,5,6],
+    ], dtype=np.int_)
+
+class TestConstraints:
+    def test_duplicate_row_insertion(self):
+        nyth_const = Constraint.from_puzzle(_nyt_hard)
+        for i, j in np.ndindex(_nyt_hard.shape):
+            if _nyt_hard[i, j] != 0:
+                for k in range(nyth_const.N):
+                    l = i*nyth_const.N + k
+                    assert not nyth_const.is_safe(l, _nyt_hard[i, j]), "Duplicate row insertion should not be allowed."
+
+    def test_duplicate_col_insertion(self):
+        nyth_const = Constraint.from_puzzle(_nyt_hard)
+        for i, j in np.ndindex(_nyt_hard.shape):
+            if _nyt_hard[i, j] != 0:
+                for k in range(nyth_const.N):
+                    l = k*nyth_const.N + j
+                    assert not nyth_const.is_safe(l, _nyt_hard[i, j]), "Duplicate col insertion should not be allowed."
+
+    def test_duplicate_box_insertion(self):
+        nyth_const = Constraint.from_puzzle(_nyt_hard)
+        for i, j in np.ndindex(_nyt_hard.shape):
+            if _nyt_hard[i, j] != 0:
+                box = nyth_const.b[i*nyth_const.N + j]
+                for k in range(nyth_const.N):
+                    for l in range(nyth_const.N):
+                        if (k//nyth_const.N != box[0] or l//nyth_const.N != box[1]):
+                            continue
+                        m = k*nyth_const.N + l
+                        assert not nyth_const.is_safe(m, _nyt_hard[i, j]), "Duplicate box insertion should not be allowed."
+
+    def test_safe_to_insert_puzzle_solution(self):
+        nyth_const = Constraint.from_puzzle(_nyt_hard)
+        for i, j in np.ndindex(_nyt_hard.shape):
+            if _nyt_hard[i, j] == 0:
+                assert nyth_const.is_safe(i*nyth_const.N + j, _nyth_sol[i, j]), "Puzzle solution should be safe to insert."
+            else:
+                assert not nyth_const.is_safe(i*nyth_const.N + j, _nyth_sol[i, j]), "It should not be safe to doubly insert a known value."
+
+    def test_safe_to_insert_full_solution(self):
+        nyth_const = Constraint.from_puzzle(_nyt_hard)
+        for i, j in np.ndindex(_nyt_hard.shape):
+            if _nyt_hard[i, j] == 0:
+                assert nyth_const.is_safe(i*nyth_const.N + j, _nyth_sol[i, j]), "Puzzle solution should be safe to insert, with constraint updating."
+                nyth_const.update(i*nyth_const.N + j, _nyt_hard[i, j])
+            else:
+                assert not nyth_const.is_safe(i*nyth_const.N + j, _nyth_sol[i, j]), "It should not be safe to doubly insert a known value."
+
+    def test_no_insertions_in_completed_puzzle(self):
+        nyth_done_const = Constraint.from_puzzle(_nyth_sol)
+        for i, j in np.ndindex(_nyt_hard.shape):
+            for k in range(nyth_done_const.N):
+                assert not nyth_done_const.is_safe(i*nyth_done_const.N + j, k+_1), "No insertions should be allowed in a completed puzzle."
+                mask = np.int_(0)
+        for l in range(1,nyth_done_const.N+1):
+            mask |= np.int_(1) << l
+        assert all(nyth_done_const.row == mask), "Row mask was not completely filled."
+        assert all(nyth_done_const.col == mask), "Col mask was not completely filled."
+        assert all(nyth_done_const.box.flatten() == mask), "Box mask was not completely filled."
+
+    def test_double_add_is_a_noop(self):
+        const = Constraint()
+        const.update((const.N**2)//2, np.int_(9))
+        const2 = copy.deepcopy(const)
+        const.update((const.N**2)//2, np.int_(9))
+        assert const == const2, "Double adding the same value should be a no-op."
+        assert all(const.row == const2.row), "Row mask should not change."
+        assert all(const.col == const2.col), "Col mask should not change."
+        assert all(const.box.flatten() == const2.box.flatten()), "Box mask should not change."
+        const.update((const.N**2)//2, np.int_(8))
+        assert const != const2, "Adding a different value should change the constraint."
+        assert not all(const.row == const2.row), "Row mask should change."
+        assert not all(const.col == const2.col), "Col mask should change."
+        assert not all(const.box.flatten() == const2.box.flatten()), "Box mask should change."
+
+    def test_double_remove_is_a_noop(self):
+        const = Constraint.from_puzzle(_nyth_sol)
+        const2 = copy.deepcopy(const)
+        const.remove((const.N**2)//2, np.int_(9))
+        assert const != const2, "Removing a value should change the constraint."
+        assert not all(const.row == const2.row), "Row mask should change."
+        assert not all(const.col == const2.col), "Col mask should change."
+        assert not all(const.box.flatten() == const2.box.flatten()), "Box mask should change."
+        const2 = copy.deepcopy(const)
+        assert const == const2, "Constraints should be the same after deep copy."
+        const.remove((const.N**2)//2, np.int_(9))
+        assert const == const2, "Double removing the same value should be a no-op."
+
+
+    def test_removal(self):
+        nyth_const = Constraint.from_puzzle(_nyt_hard)
+        nyth_sol_const = Constraint.from_puzzle(_nyth_sol)
+        assert nyth_const != nyth_sol_const, "Solution constraint should be different from the original constraint."
+        for i, j in np.ndindex(_nyt_hard.shape):
+            if _nyt_hard[i, j] != 0:
+                assert _nyt_hard[i, j] == _nyth_sol[i, j], "Puzzle and solution should match for known values."
+                const1 = copy.deepcopy(nyth_sol_const)
+                assert not nyth_sol_const.is_safe(i*nyth_sol_const.N + j, _nyt_hard[i, j]), "It should not be safe to reinsert a known value."
+                nyth_sol_const.remove(i*nyth_sol_const.N + j, _nyt_hard[i, j])
+                print(nyth_sol_const)
+                assert nyth_sol_const.is_safe(i*nyth_sol_const.N + j, _nyth_sol[i, j]), "Puzzle solution should be safe to insert after item removed."
+                nyth_sol_const.update(i*nyth_sol_const.N + j, _nyth_sol[i, j])
+                assert not nyth_sol_const.is_safe(i*nyth_sol_const.N + j, _nyt_hard[i, j]), "Backtracking restoration of constraint should allow reinsertion and detect attempted duplicate."
+                print(const1)
+                print(nyth_sol_const)
+                assert const1 == nyth_sol_const, "Restored constraint should be the same as the original constraint."
+        for i, j in np.ndindex(_nyt_hard.shape):
+            if _nyt_hard[i, j] == 0:
+                nyth_sol_const.remove(i*nyth_sol_const.N + j, _nyth_sol[i, j])
+                assert nyth_sol_const.is_safe(i*nyth_sol_const.N + j, _nyt_hard[i, j]), "Puzzle solution should be safe to insert after item removed."
+        assert nyth_sol_const == Constraint.from_puzzle(_nyt_hard), "Solution constraint should be the same as the original constraint after removal and reinsertion of solved cells."
+        assert nyth_const == nyth_sol_const, "Solution constraint should be the same as the original constraint after removal of solved cells."
